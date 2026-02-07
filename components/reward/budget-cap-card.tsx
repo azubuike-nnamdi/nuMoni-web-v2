9cd43606e5f7f2978e12f70ff370baace5b54842
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2026-02-07 07:36:20 +0100

    style: update styles :paintbrush:

--- components/reward/budget-cap-card.tsx
+++ components/reward/budget-cap-card.tsx
@@ -1,7 +1,6 @@
 import { cashBackIcon } from "@/constant/icons";
 import { formatNumber } from "@/lib/helper";
 import Image from "next/image";
-import { GraphDirectionIcon } from "../common/icon-svg";
 import CardSkeleton from "./card-skeleton";
 
 interface BudgetCapCardProps {
@@ -9,13 +8,13 @@ interface BudgetCapCardProps {
   isLoading?: boolean;
 }
 
-export default function BudgetCapCard({ rewardCap, isLoading }: BudgetCapCardProps) {
+export default function BudgetCapCard({ rewardCap, isLoading }: Readonly<BudgetCapCardProps>) {
   if (isLoading) {
     return <CardSkeleton variant="gradient" />;
   }
 
   return (
-    <div className="relative overflow-hidden bg-gradient-to-br from-green-800 to-green-900 text-white rounded-xl p-6 flex flex-col h-full">
+    <div className="relative overflow-hidden bg-linear-to-br from-green-800 to-green-900 text-white rounded-xl p-6 flex flex-col h-full">
       {/* Cash Back Background Icon */}
       <div className="absolute -top-8 -right-16 opacity-30">
         <Image src={cashBackIcon} alt="Cash Back" width={200} height={200} />
@@ -29,13 +28,13 @@ export default function BudgetCapCard({ rewardCap, isLoading }: BudgetCapCardPro
       </div>
       <div className="relative z-10">
         <hr className="border-white/10 mb-4" />
-        <div className="flex items-center gap-2">
+        {/* <div className="flex items-center gap-2">
           <div className="bg-white rounded-full px-2 py-1 flex items-center gap-1">
             <GraphDirectionIcon className="w-3 h-3 bg-theme-dark-green rounded-full p-0.5 text-white" />
             <span className="text-xs font-semibold text-theme-dark-green">+20%</span>
           </div>
           <span className="text-xs text-white font-semibold">Increase Than Last Month</span>
-        </div>
+        </div> */}
       </div>
     </div>
   );
