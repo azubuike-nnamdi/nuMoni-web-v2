86664fc7549ed4f205b9a085e092b8ecd823d90e
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2026-02-09 15:51:54 +0100

    chore: set expiration to never of 10000days :fire:

--- components/reward/sections/expiration-section.tsx
+++ components/reward/sections/expiration-section.tsx
@@ -4,15 +4,15 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import { ExpirationSectionProps } from "@/lib/types";
 
 
-export default function ExpirationSection({ pointExpiration, setPointExpiration }: ExpirationSectionProps) {
+export default function ExpirationSection({ pointExpiration, setPointExpiration }: Readonly<ExpirationSectionProps>) {
   return (
     <div className="space-y-2">
-      <label className="text-sm font-semibold text-gray-900 mb-3">
+      <label htmlFor="point-expiration" className="text-sm font-semibold text-gray-900 mb-3">
         Point Expiration Before Next Purchase <span className="text-gray-500">(Optional)</span>
       </label>
-      <Select value={pointExpiration} onValueChange={setPointExpiration}>
+      <Select value={pointExpiration || "10000days"} onValueChange={setPointExpiration} disabled>
         <SelectTrigger className="w-full py-6 mt-2">
-          <SelectValue placeholder="This Means Each Point Claimed By A User Will Expire If They Do Not Spend It" className="placeholder:text-sm" />
+          <SelectValue placeholder="Never" className="placeholder:text-sm" />
         </SelectTrigger>
         <SelectContent>
           <SelectItem value="1-day">1 Day</SelectItem>
@@ -20,7 +20,7 @@ export default function ExpirationSection({ pointExpiration, setPointExpiration 
           <SelectItem value="7-days">7 Days</SelectItem>
           <SelectItem value="14-days">14 Days</SelectItem>
           <SelectItem value="30-days">30 Days</SelectItem>
-          <SelectItem value="never">Never</SelectItem>
+          <SelectItem value="10000days">Never</SelectItem>
         </SelectContent>
       </Select>
     </div>
